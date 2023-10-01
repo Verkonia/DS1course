d58fdbf0a988f9f98c28837c12406ce514c751f3
Verkonia <email@example.ru>
2023-10-01 14:16:00 +0300

    add my_game

---
+++ project_1/my_game.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 1
+    predict_number = 50
+    while predict_number != number:
+        while  predict_number > number:
+            predict_number -= 10
+            count += 1
+            while predict_number < number:
+                predict_number += 1
+                count += 1
+        while  predict_number < number:
+            predict_number += 10
+            count += 1
+            while predict_number > number:
+                predict_number -= 1
+                count += 1
+    return count
+
+def score_game(game_core_v3) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        game_core_v3 ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v3(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(game_core_v3)
+
+score_game(game_core_v3)
